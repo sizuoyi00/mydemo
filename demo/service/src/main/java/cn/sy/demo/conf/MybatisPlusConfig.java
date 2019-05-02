b328cf862cb32a9f3264e3ba339ae6cbc695ff3a
sizuoyi <sizuoyi@jf180.cn>
2019-05-03 02:57:30 +0800

    mybatis-plus多数据源补充  暂时没有搞分布式事务，等mybatis-plus完善后有时间再加上

--- demo/service/src/main/java/cn/sy/demo/conf/MybatisPlusConfig.java
+++ demo/service/src/main/java/cn/sy/demo/conf/MybatisPlusConfig.java
@@ -1,17 +1,45 @@
 package cn.sy.demo.conf;
 
+
+//import com.baomidou.mybatisplus.dts.EnableDtsRabbit;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@MapperScan("cn.sy.demo.dao,demo")
+//@EnableDtsRabbit
+@MapperScan("cn.sy.demo.mapper")
 public class MybatisPlusConfig {
+
 
     /**
      * 分页插件
+     * @return
      */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 乐观锁配置
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+//    /**
+//     * 分布式事务 rabbitmq
+//     * @param connectionFactory
+//     * @return
+//     */
 //    @Bean
-//    public PaginationInterceptor paginationInterceptor() {
-//        return new PaginationInterceptor();
+//    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+//        return new RabbitAdmin(connectionFactory);
 //    }
 
 }
